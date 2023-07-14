f49f9e08637dccdb33c7946665929223af95d74c
Ilyas Khasakhanov <clauster27@gmail.com>
2023-07-14 20:00:37 +0400

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import Flask 
+class Point:
+    def __init__(self, name=None):
+        if not isinstance(name, str):
+            raise TypeError('Неверное имя')
+        self.__name = name
+    
 
-fl_link = Flask()
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, new):
+        if not isinstance(new, str):
+            raise TypeError('Неверное имя')
+        self.__name = new
+
+
+p = Point('Ильяс')
+print(p.name)
