11091b9d9ac2e3a47b8259d9b04f9bffeed792d1
Ilyas Khasakhanov <clauster27@gmail.com>
2023-07-14 21:16:38 +0400

    end_lesson

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 class Point:
+    __instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
     def __init__(self, name=None):
         if not isinstance(name, str):
             raise TypeError('Неверное имя')
@@ -11,6 +17,7 @@ class Point:
     
     @name.setter
     def name(self, new):
+        print(new)
         if not isinstance(new, str):
             raise TypeError('Неверное имя')
         self.__name = new
